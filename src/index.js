0db5baa7bbe0590c3b6256f7a98c5d2c47198ebc
nstoman <nangialaistoman7@gmail.com>
2020-12-22 22:56:20 +0430

    Working feather icons

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@
 import 'popper.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap';
-import 'feather-icons';
 import './sass/app.scss';
-
 import { stoman } from './js/app';
 
 document.querySelector('h1').textContent = stoman('Hi, how are you?');
+
+// Call FeatherIcons method in order to use its icons
+const feather = require('feather-icons');
+feather.replace();
