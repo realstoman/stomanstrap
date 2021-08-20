98f61af456837ee590e76d0121fdc2301325cb18
nstoman <nangialaistoman7@gmail.com>
2021-08-20 20:08:11 +0430

    Banner design

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import 'animate.css';
 import './sass/app.scss';
 import { stoman } from './js/app';
 
-document.querySelector('h1').textContent = stoman('Hi, how are you?');
+// document.querySelector('h1').textContent = stoman('Hi, how are you?');
 
 // Call FeatherIcons method in order to use its icons
 const feather = require('feather-icons');
